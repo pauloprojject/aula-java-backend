f7e53834187187e3073eca1c61387b0af5db91ea
pauloprojject <paulo8512@gmail.com>
2022-09-19 21:36:49 -0300

    requestbody nas requisições

--- src/main/java/br/iesp/edu/api/resource/FilmeResource.java
+++ src/main/java/br/iesp/edu/api/resource/FilmeResource.java
@@ -3,18 +3,19 @@ package br.iesp.edu.api.resource;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.iesp.edu.api.entity.Filme;
 import br.iesp.edu.api.service.FilmeService;
 
-@Controller
+@RestController
 @RequestMapping("/filme")
 public class FilmeResource {
     
@@ -22,17 +23,17 @@ public class FilmeResource {
     private FilmeService service;
 
     @PostMapping
-    public Filme salvar(Filme filme){
+    public Filme salvar(@RequestBody Filme filme){
         return service.salvar(filme);
     }
 
     @PutMapping
-    public Filme atualizar(Filme filme) throws Exception{
+    public Filme atualizar(@RequestBody Filme filme) throws Exception{
         return service.atualizar(filme);
     }
 
     @DeleteMapping
-    public void excluir(Filme filme) throws Exception{
+    public void excluir(@RequestBody Filme filme) throws Exception{
         service.deletar(filme);
     }
 
